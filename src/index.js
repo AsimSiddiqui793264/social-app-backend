9d6c09c9a149b2bebfd52e69eb2dc0353eead232
AsimSiddiqui793264 <asimsiddiqui939@gmail.com>
2026-09-07 22:43:30 -0700

    fix: correct start script and improve server log message

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import dotenv from "dotenv";
 
 dotenv.config()
 
-const PORT = process.env.PORT 
+const PORT = process.env.PORT || 5000;
 
 connectDB()
 .then(() => {
-    app.listen(PORT , () => {
-        console.log(`Server is running on : http://localhost:${PORT}`);
-    });
+    app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 })
 .catch((error) => {
     console.log("MongoDB connection error is : " , error);
